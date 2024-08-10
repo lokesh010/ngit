f14cff114d2bab94c6a379ac8c1921b78733c5ec
lokesh bajracharya <lokesh.bajracharya55@gmail.com>
2024-08-10 15:43:53 +0545

    output dir for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./dist",
+  },
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
